e4e6f982a5fe95817e7a0b2fabe495ca69eb0489
QuocAnh189 <anhquoc18092003@gmail.com>
2023-12-05 15:35:06 +0700

    edit

--- src/app/api/create_table_ex/route.ts
+++ src/app/api/create_table_ex/route.ts
@@ -1,14 +1,15 @@
-// import { sql } from '@vercel/postgres';
-// import { NextResponse } from 'next/server';
-// import { revalidatePath } from 'next/cache';
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache';
 
-// export async function GET() {
-//     try {
-//         const result = await sql`CREATE TABLE myblog (_id varchar(255), avatar varchar(255), name varchar(255), description varchar(255), age int, dislike boolean ,CreatedAt TIMESTAMP );`;
-//         revalidatePath('/page/home');
-//         return NextResponse.json({ result }, { status: 200 });
-//     } catch (error) {
-//         console.log(error);
-//         return NextResponse.json({ error }, { status: 500 });
-//     }
-// }
+export async function GET() {
+    try {
+        const result = await sql`CREATE TABLE myblog (_id varchar(255), avatar varchar(255), name varchar(255), description varchar(255), age int, dislike boolean ,CreatedAt TIMESTAMP );`;
+        revalidatePath('/page/home');
+        return NextResponse.json({ result }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error }, { status: 500 });
+    }
+}
+        
--- src/app/api/delete_table_ex/route.ts
+++ src/app/api/delete_table_ex/route.ts
@@ -1,15 +1,15 @@
-// import { sql } from '@vercel/postgres';
-// import { revalidatePath } from 'next/cache';
-// import { NextResponse } from 'next/server';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { NextResponse } from 'next/server';
 
 
-// export async function GET() {
-//     try {
-//         await sql`DROP TABLE myblog`;
-//         revalidatePath('/page/home');
-//         return NextResponse.json({ message: 'Delete Successfully' }, { status: 200 });
-//     } catch (error) {
-//         console.log(error);
-//         return NextResponse.json({ error }, { status: 500 });
-//     }
-// }
+export async function GET() {
+    try {
+        await sql`DROP TABLE myblog`;
+        revalidatePath('/page/home');
+        return NextResponse.json({ message: 'Delete Successfully' }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error }, { status: 500 });
+    }
+}
